aafc30a57cfe04aa0e96c02222b6b18c5d97e5e2
susanlin0705 <susan.lin0705@gmail.com>
2019-08-06 13:57:12 -0500

    better-fix

--- src/Author.java
+++ src/Author.java
@@ -3,13 +3,12 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
@@ -29,7 +28,7 @@ public class Author extends Person {
     }
 
     @Override
-    public String sortName() {
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
