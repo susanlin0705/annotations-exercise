e2d0920c581138067e98264e62a00008f455656a
susanlin0705 <susan.lin0705@gmail.com>
2019-08-06 12:15:02 -0500

    fix error

--- src/Author.java
+++ src/Author.java
@@ -4,6 +4,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
@@ -19,6 +20,7 @@ public class Author extends Person {
     public List<String> getBooks() {
         return books;
     }
+
 
     public List<String> publishedBooks() {
         return books;
